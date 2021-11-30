7560a587f987e4cd9e04dc94e3b71d1ed6301bf3
jonsamp <sampjon@gmail.com>
2021-11-30 12:01:19 -0500

    update user facing version

--- src/scenes/Settings/SettingsDetail.tsx
+++ src/scenes/Settings/SettingsDetail.tsx
@@ -187,7 +187,7 @@ function SettingsDetail(props: Props) {
           </Section>
           <Section>
             <SettingWrapper title="Version">
-              <Text style={tw('body theme.text.default')}>43 (3)</Text>
+              <Text style={tw('body theme.text.default')}>43 (4)</Text>
             </SettingWrapper>
           </Section>
         </>
