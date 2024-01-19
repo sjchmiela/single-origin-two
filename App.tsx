a7b9a4a54dabad7826450f691d608c06d99322d4
Brent Vatne <brentvatne@gmail.com>
2024-01-19 13:25:32 -0800

    Attempt SDK 50 upgrade

--- App.tsx
+++ App.tsx
@@ -1,87 +1,106 @@
-import * as Device from 'expo-device';
-import * as Font from 'expo-font';
-import * as ScreenOrientation from 'expo-screen-orientation';
-import * as SplashScreen from 'expo-splash-screen';
+// Temporarily blank App.tsx in order to 
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { Animated } from 'react-native';
-import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
-import { enableScreens } from 'react-native-screens';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { TailwindProvider } from 'tailwind-rn';
-
-import Navigator from './src/navigation';
-import configureStore from './src/store/configureStore';
-import utilities from './tailwind.json';
-
-enableScreens();
-
-const { store, persistor } = configureStore();
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        const deviceType = await Device.getDeviceTypeAsync();
-        if (deviceType === Device.DeviceType.TABLET) {
-          await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT);
-        }
-
-        // Keep the splash screen visible while we fetch resources
-        await SplashScreen.preventAutoHideAsync();
-
-        await Font.loadAsync({
-          Script: require('./assets/SignPainter-HouseScript.ttf'),
-        });
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      Animated.timing(fadeAnim, {
-        toValue: 1,
-        duration: 666,
-        useNativeDriver: true,
-      }).start();
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
   return (
-    <Provider store={store}>
-      <TailwindProvider utilities={utilities}>
-        <StatusBar style="auto" />
-        <PersistGate loading={null} persistor={persistor}>
-          <SafeAreaProvider
-            style={{ backgroundColor: 'black' }}
-            initialMetrics={initialWindowMetrics}>
-            <Animated.View style={{ opacity: fadeAnim, flex: 1 }} onLayout={onLayoutRootView}>
-              <Navigator />
-            </Animated.View>
-          </SafeAreaProvider>
-        </PersistGate>
-      </TailwindProvider>
-    </Provider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// import * as Device from 'expo-device';
+// import * as Font from 'expo-font';
+// import * as ScreenOrientation from 'expo-screen-orientation';
+// import * as SplashScreen from 'expo-splash-screen';
+// import { StatusBar } from 'expo-status-bar';
+// import React, { useState, useEffect, useRef, useCallback } from 'react';
+// import { Animated } from 'react-native';
+// import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { TailwindProvider } from 'tailwind-rn';
+
+// import Navigator from './src/navigation';
+// import configureStore from './src/store/configureStore';
+// import utilities from './tailwind.json';
+
+// const { store, persistor } = configureStore();
+
+// export default function App() {
+//   const [appIsReady, setAppIsReady] = useState(false);
+//   const fadeAnim = useRef(new Animated.Value(0)).current;
+
+//   useEffect(() => {
+//     async function prepare() {
+//       try {
+//         const deviceType = await Device.getDeviceTypeAsync();
+//         if (deviceType === Device.DeviceType.TABLET) {
+//           await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT);
+//         }
+
+//         // Keep the splash screen visible while we fetch resources
+//         await SplashScreen.preventAutoHideAsync();
+
+//         await Font.loadAsync({
+//           Script: require('./assets/SignPainter-HouseScript.ttf'),
+//         });
+//       } catch (e) {
+//         console.warn(e);
+//       } finally {
+//         // Tell the application to render
+//         setAppIsReady(true);
+//       }
+//     }
+
+//     prepare();
+//   }, []);
+
+//   const onLayoutRootView = useCallback(async () => {
+//     if (appIsReady) {
+//       // This tells the splash screen to hide immediately! If we call this after
+//       // `setAppIsReady`, then we may see a blank screen while the app is
+//       // loading its initial state and rendering its first pixels. So instead,
+//       // we hide the splash screen once we know the root view has already
+//       // performed layout.
+//       Animated.timing(fadeAnim, {
+//         toValue: 1,
+//         duration: 666,
+//         useNativeDriver: true,
+//       }).start();
+//       await SplashScreen.hideAsync();
+//     }
+//   }, [appIsReady]);
+
+//   if (!appIsReady) {
+//     return null;
+//   }
+
+//   return (
+//     <Provider store={store}>
+//       <TailwindProvider utilities={utilities}>
+//         <StatusBar style="auto" />
+//         <PersistGate loading={null} persistor={persistor}>
+//           <SafeAreaProvider
+//             style={{ backgroundColor: 'black' }}
+//             initialMetrics={initialWindowMetrics}>
+//             <Animated.View style={{ opacity: fadeAnim, flex: 1 }} onLayout={onLayoutRootView}>
+//               <Navigator />
+//             </Animated.View>
+//           </SafeAreaProvider>
+//         </PersistGate>
+//       </TailwindProvider>
+//     </Provider>
+//   );
+// }
--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default ({ config } = {}) => ({
       // This is Jon's id
       // "projectId": "675cb1f0-fa3c-11e8-ac99-6374d9643cb2"
       // This is my own id
-      "projectId": "9b79fc2e-de92-46d6-8442-33b4eb72b8de"
+      // "projectId": "9b79fc2e-de92-46d6-8442-33b4eb72b8de"
     }
   }
 });
