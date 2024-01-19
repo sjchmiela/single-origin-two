442d6a6ff760b68eb6eb55ce78568522bb32f591
Brent Vatne <brentvatne@gmail.com>
2024-01-19 13:05:36 -0800

    Get it running on iOS

---
+++ app.config.js
@@ -0,0 +1,12 @@
+export default ({ config } = {}) => ({
+  ...config,
+  "extra": {
+    "eas": {
+      // TODO: read from env?
+      // This is Jon's id
+      // "projectId": "675cb1f0-fa3c-11e8-ac99-6374d9643cb2"
+      // This is my own id
+      "projectId": "9b79fc2e-de92-46d6-8442-33b4eb72b8de"
+    }
+  }
+});
