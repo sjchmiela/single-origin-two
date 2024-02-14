0be27e29bfaee28d8a71d574fe122ea74e52e865
Brent Vatne <brentvatne@gmail.com>
2024-02-13 16:55:12 -0800

    Better for publish consumption

--- app.config.js
+++ app.config.js
@@ -1,12 +1,10 @@
+const OFFICIAL_PROJECT_ID = "675cb1f0-fa3c-11e8-ac99-6374d9643cb2";
+
 export default ({ config } = {}) => ({
   ...config,
   "extra": {
     "eas": {
-      // TODO: read from env?
-      // This is Jon's id
-      // "projectId": "675cb1f0-fa3c-11e8-ac99-6374d9643cb2"
-      // This is my own id
-      // "projectId": "9b79fc2e-de92-46d6-8442-33b4eb72b8de"
+      "projectId": process.env.EAS_PROJECT_ID ?? OFFICIAL_PROJECT_ID
     }
   }
 });
