21cf73624bad3d4be6ce3be641110de1715a896d
Jon Samp <sampjon@gmail.com>
2024-05-01 23:04:35 -0500

    upgrades to sdk 51

--- app.config.js
+++ app.config.js
@@ -1,10 +1,11 @@
-const OFFICIAL_PROJECT_ID = "675cb1f0-fa3c-11e8-ac99-6374d9643cb2";
+const OFFICIAL_PROJECT_ID = '675cb1f0-fa3c-11e8-ac99-6374d9643cb2';
 
 export default ({ config } = {}) => ({
   ...config,
-  "extra": {
-    "eas": {
-      "projectId": process.env.EAS_PROJECT_ID ?? OFFICIAL_PROJECT_ID
-    }
-  }
+  extra: {
+    eas: {
+      projectId: process.env.EAS_PROJECT_ID ?? OFFICIAL_PROJECT_ID,
+    },
+  },
+  plugins: ['expo-font'],
 });
