df31818d5a7fa2645051064aa3c7f00ee9a8ffdc
Jon Samp <sampjon@gmail.com>
2024-10-04 00:03:15 -0500

    updates project id

--- app.config.js
+++ app.config.js
@@ -1,12 +1,18 @@
 const OFFICIAL_PROJECT_ID = '675cb1f0-fa3c-11e8-ac99-6374d9643cb2';
 const PREVIEW_PROJECT_ID = 'e7f3dda2-c182-4f86-bae0-9c389b4add3f';
+const PROJECT_ID = process.env.EAS_PROJECT_ID ?? PREVIEW_PROJECT_ID;
 
 export default ({ config } = {}) => ({
   ...config,
   extra: {
     eas: {
-      projectId: process.env.EAS_PROJECT_ID ?? PREVIEW_PROJECT_ID,
+      projectId: PROJECT_ID,
     },
+  },
+  updates: {
+    enabled: true,
+    fallbackToCacheTimeout: 3000,
+    url: `https://u.expo.dev/${PROJECT_ID}`,
   },
   plugins: [
     [
