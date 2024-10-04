8eb72443d6904afd8b59c2f3112cbfc6ff811abe
Jon Samp <sampjon@gmail.com>
2024-10-03 23:52:29 -0500

    adds preview id

--- app.config.js
+++ app.config.js
@@ -1,10 +1,11 @@
 const OFFICIAL_PROJECT_ID = '675cb1f0-fa3c-11e8-ac99-6374d9643cb2';
+const PREVIEW_PROJECT_ID = 'e7f3dda2-c182-4f86-bae0-9c389b4add3f';
 
 export default ({ config } = {}) => ({
   ...config,
   extra: {
     eas: {
-      projectId: process.env.EAS_PROJECT_ID ?? OFFICIAL_PROJECT_ID,
+      projectId: process.env.EAS_PROJECT_ID ?? PREVIEW_PROJECT_ID,
     },
   },
   plugins: [
